17474d8a63af7577b9a5bfd49c5c82e9f04862ba
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2022-12-14 15:15:44 +0100

    esercizio completato

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("ok java")
+
+const lista = ["pane" , "pasta" , "uova"];
+const target = document.getElementById("target");
+let contatore = 0;
+let testoFinale = "<ul>" ;
+
+while (contatore < lista.length){
+
+    testoFinale += `<li> ${lista[contatore]} </li>`
+    contatore++
+
+}
+
+testoFinale += "</ul>";
+target.innerHTML = testoFinale;
